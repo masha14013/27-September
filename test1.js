5150b3acefd6031c42b585ba2551cf256e903ccf
masha14013 <masha14013@rambler.ru>
2022-09-27 10:52:24 +0300

    added file test1.js

---
+++ test1.js
@@ -0,0 +1,19 @@
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  let a = a2;
+  let D = d1;
+  let d = d2;
+  return d1+a2+d2;
+}
+function Bee(){
+  let B = b1;
+  let b = b2;
+  let e = e2
+  return b1+e2+e2;
+}
+function banana(){
+  let a = a2;
+  let n = n2;
+  let b = b2;
+  return b2+a2+n2+a2+n2+a2;
+}
