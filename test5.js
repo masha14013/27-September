ed09f7dd2ea0bdbb7a4a27cb8c961bfc67a914a5
masha14013 <masha14013@rambler.ru>
2022-09-27 11:17:11 +0300

    added file test5.js

---
+++ test5.js
@@ -0,0 +1,15 @@
+function howManydays(month){
+  var days;
+  switch (month){
+    case 2:
+      return 28;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+    default:
+      return 31;
+  }
+  return days;
+}
